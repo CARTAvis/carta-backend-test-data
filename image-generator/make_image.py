a69c6f668aacfd270cd7c25b2bc0840f14c13445
Michaela van Zyl <michaela@maholla.com>
2026-01-15 12:09:30 +0200

    another generator adjustment

--- image-generator/make_image.py
+++ image-generator/make_image.py
@@ -60,8 +60,8 @@ def fits_to_hdf5(fits_path, hdf5_path):
     with fits.open(fits_path) as hdul:
         with h5py.File(hdf5_path, "w") as h5f:
             for i, hdu in enumerate(hdul):
-                # Create group for each HDU with HDU_i naming convention
-                grp = h5f.create_group(f"HDU_{i}")
+                # Create group for each HDU using numeric naming convention
+                grp = h5f.create_group(str(i))
 
                 # Write FITS data (if present) with lowercase 'data' name
                 if hdu.data is not None:
